753fe53ecf0258f9aa01c7105b6258fc221db2be
Blaine Lafreniere <blafreniere@deseretbook.com>
2023-11-16 19:37:52 -0700

    finish CRUD functionality for Planes entity

--- controllers/planes.mjs
+++ controllers/planes.mjs
@@ -1,7 +1,16 @@
 const select_plane_with_model = `SELECT * FROM Planes JOIN Models ON Planes.model_id = Models.model_id WHERE plane_id = ? `
 const delete_plane = `DELETE FROM Planes WHERE plane_id = ?`
+const update_plane = `UPDATE Planes SET model_id = ?, mileage = ? WHERE plane_id = ?`
+
+// for dropdown
+const select_models = `SELECT * FROM Models`
 
 function planes_controller(app, db) {
+
+  // ---------
+  // index
+  // ---------
+
   app.get('/planes', async (req, res) => {
     const select_planes = 'SELECT * FROM Planes JOIN Models ON Planes.model_id = Models.model_id'
     try {
@@ -12,6 +21,10 @@ function planes_controller(app, db) {
     }
   })
 
+  // ---------
+  // new / create
+  // ---------
+
   app.post('/planes', async (req, res) => {
     const insert_plane_query = `INSERT INTO Planes (model_id, mileage) VALUES (?, ?)`
     const model_id = req.body.model_id
@@ -21,11 +34,33 @@ function planes_controller(app, db) {
   })
 
   app.get('/planes/new', async (req, res) => {
-    const select_models = `SELECT * FROM Models`
     const models = await db.awaitQuery(select_models)
     res.render('planes/new', {models})
   })
 
+  // ---------
+  // edit / update
+  // ---------
+
+  app.get('/planes/:plane_id/edit', async (req, res) => {
+    const plane_id = req.params.plane_id
+    const results = await db.awaitQuery(select_plane_with_model, plane_id)
+    const models = await db.awaitQuery(select_models)
+    const plane = results[0]
+    res.render('planes/edit', {plane, models})
+  })
+
+  app.post('/planes/:plane_id/update', async (req, res) => {
+    const plane_id = req.params.plane_id
+    const model_id = req.body.model_id
+    const mileage = req.body.mileage
+    await db.awaitQuery(update_plane, [model_id, mileage, plane_id])
+    res.status(204).redirect(301, '/planes')
+  })
+
+  // ---------
+  // delete / confirm delete
+  // ---------
   app.get('/planes/:plane_id/confirm_delete', async (req, res) => {
     const plane_id = req.params.plane_id
     const results = await db.awaitQuery(select_plane_with_model, plane_id)
